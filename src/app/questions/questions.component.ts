a885f55f06dc3818d86297ee91285aa300ccf1d8
Joel Philip <joelphilipkon@gmail.com>
2021-11-09 16:39:39 +0530

    next button works

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -9,14 +9,25 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./questions.component.css']
 })
 export class QuestionsComponent implements OnInit {
+  question_count = 0;
+  questions: any = [];
   quizzes : Array<Quiz>= []
-  constructor(private quizService: QuizserviceService) { }
+  constructor(private quizService: QuizserviceService, private _router: ActivatedRoute) { }
 
   ngOnInit(): void { 
+    this.questions = [];
     this.quizService.fetchAllQuizzes()
     .subscribe((res:any) => {
-      this.quizzes = res;
+      // this.quizzes = res;
+    this.questions = res
+    console.log(this.questions);
     });
+  }
+  next(){
+    this.question_count++;
+  }
+  previous(){
+    this.question_count--;
   }
   }
   
